8b33e0c31265d56ddcfabcd28043beab627646ed
ISMATULLAH <guldastaislamorquran@gmail.com>
2026-09-26 00:45:44 +0500

    Update cutecut-pro.rb

--- homebrew/Casks/c/cutecut-pro.rb
+++ homebrew/Casks/c/cutecut-pro.rb
@@ -1,12 +1,14 @@
 cask "cutecut-pro" do
   version "2.5.0"
-  sha256 :no_check
+  sha256 :bdd0efda0b84ae4ca41857801ce7652c2e7ac7471aac2a9e94b61d95e8b36572
 
   url "https://github.com/MDIsmatullah/CuteCut-Pro/releases/download/v#{version}/CuteCut.Pro-#{version}-arm64.dmg"
   name "CuteCut Pro"
   desc "Professional multitrack video editor with Quranic audio-to-text synchronization"
   homepage "https://github.com/MDIsmatullah/CuteCut-Pro"
 
+depends_on arch: :arm64
+  
   livecheck do
     url :url
     strategy :github_latest
